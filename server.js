182e399f855fb5a07bf36457cdc73c06e4cf3ac4
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-11-04 17:36:09 -0600

    change result from function

--- server.js
+++ server.js
@@ -22,75 +22,33 @@ app.post('/geoCode', (req, res, next) => {
   const key = process.env.KEY;
   const locations = req.body.locations;
 
-  // const search = async () => {
-  // async function search() {
-  //   return locations.map(city => {
-  //     // build first url with key and location data
-  //     const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${key}`;
-  //
-  //     return fetch(url).then(res => res.json()).then(geoCode => {
-  //       // take values for lat and lng and build url with them
-  //       const lat = geoCode.results[0].geometry.location.lat;
-  //       const lng = geoCode.results[0].geometry.location.lng;
-  //       const url2 = `https://maps.googleapis.com/maps/api/elevation/json?locations=${lat},${lng}&key=${key}`;
-  //       return fetch(url2).then(response => response.json()).then(elev => {
-  //         const elevation = elev.results[0].elevation;
-  //         // elevations.push([city, elevation]);
-  //         return [city, elevation];
-  //       });
-  //     });
-  //   });
-  // }
+  function search() {
+    return new Promise((resolve, reject) => {
+      const elevations = locations.map(city => {
+        // build first url with key and location data
+        const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${key}`;
 
-    // const elevations = locations.map(city => {
-    //   // build first url with key and location data
-    //   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${key}`;
-    //
-    //   return fetch(url).then(res => res.json()).then(geoCode => {
-    //     // take values for lat and lng and build url with them
-    //     const lat = geoCode.results[0].geometry.location.lat;
-    //     const lng = geoCode.results[0].geometry.location.lng;
-    //     const url2 = `https://maps.googleapis.com/maps/api/elevation/json?locations=${lat},${lng}&key=${key}`;
-    //     return fetch(url2).then(response => response.json()).then(elev => {
-    //       const elevation = elev.results[0].elevation;
-    //       // elevations.push([city, elevation]);
-    //       return [city, elevation];
-    //     });
-    //   });
-    // });
-
-     function search() {
-      return new Promise((resolve, reject) => {
-        const elevations = locations.map(city => {
-          // build first url with key and location data
-          const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${key}`;
-
-          return fetch(url).then(res => res.json()).then(geoCode => {
-            // take values for latitude and longitude and build url with them
-            const lat = geoCode.results[0].geometry.location.lat;
-            const lng = geoCode.results[0].geometry.location.lng;
-            const url2 = `https://maps.googleapis.com/maps/api/elevation/json?locations=${lat},${lng}&key=${key}`;
-            return fetch(url2).then(response => response.json()).then(elev => {
-              const elevation = elev.results[0].elevation;
-              // elevations.push([city, elevation]);
-              // console.log(elevations);
-
-              return [city, elevation];
-            });
+        return fetch(url).then(res => res.json()).then(geoCode => {
+          // take values for latitude and longitude and build url with them
+          const lat = geoCode.results[0].geometry.location.lat;
+          const lng = geoCode.results[0].geometry.location.lng;
+          const url2 = `https://maps.googleapis.com/maps/api/elevation/json?locations=${lat},${lng}&key=${key}`;
+          return fetch(url2).then(response => response.json()).then(elev => {
+            const elevation = elev.results[0].elevation;
+            return [city, elevation];
           });
         });
-
-        // setTimeout(function(){
-          resolve(elevations);
-        // }, 1000);
       });
-    }
 
-    search().then((ele) => {
-      console.log(ele);
+      setTimeout(function(){
+        resolve(elevations);
+      }, 1000);
     });
-    // console.log(elevations);
-    // res.send(JSON.stringify(elevations));
+  }
+
+  search().then((elevations) => {
+    res.send(JSON.stringify(elevations));
+  });
 });
 
 app.use(express.static(__dirname + '/public'));
