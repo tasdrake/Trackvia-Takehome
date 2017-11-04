117b371a7c16043d7c80e403e6e8a846aa029ad3
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-11-04 11:58:17 -0600

    Added locations

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import '../index.css';
-const geoKey = 'AIzaSyAnY-2g8Lvk8QxLUA9ZzmJPQQCZ40BrQLE';
-const elevationKey = 'AIzaSyBs_fxmu5CtvqhO9FKuXXQucXL0urSDq7c';
+const key = 'AIzaSyCR170bZ7FgpB7JdxPwS2hzAUSmyfEhS64';
 
 class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
       newLocation: '',
-      locations: []
+      locations: [
+        "1675 Larimer St, Denver, CO ",
+        "33 E Quay Rd, Key West, FL 33040 ",
+        "630 Williams St NW, Atlanta, GA "],
+      elevations: [],
     };
   }
 
@@ -25,14 +28,21 @@ class Search extends Component {
 
   search = (e) => {
     e.preventDefault();
+
     this.state.locations.map(city => {
-      const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${geoKey}`;
+      const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${key}`;
 
       fetch(url).then(res => res.json()).then(geoCode => {
-        console.log(geoCode.results[0].geometry.location);
         const lat = geoCode.results[0].geometry.location.lat;
         const lng = geoCode.results[0].geometry.location.lng;
-        console.log(lat, lng);
+        const url2 = `https://maps.googleapis.com/maps/api/elevation/json?locations=${lat},${lng}&key=${key}`;
+
+        fetch(url2).then(response => response.json()).then(elev => {
+          const elevations = this.state.elevations;
+          const elevation = elev.results[0].elevation;
+          elevations.push([city, elevation]);
+          this.setState({ elevations });
+        });
       });
 
 
@@ -40,43 +50,47 @@ class Search extends Component {
   }
 
 
+
   render() {
     return (
-      <form className="form-horizontal well">
-        <div className="form-group">
-          <div className="col-sm-8 col-sm-offset-2">
-            <h4>Add Locations to Elevation Search</h4>
+      <div>
+        <form className="form-horizontal well">
+          <div className="form-group">
+            <div className="col-sm-8 col-sm-offset-2">
+              <h4>Add Locations to Elevation Search</h4>
+            </div>
           </div>
-        </div>
-        <div className="form-group">
-          <label className="col-sm-2 control-label">New Location</label>
-          <div className="col-sm-12">
-            <div className="row">
-              <div className="col-sm-4">
-                <input type="text" className="form-control" onChange={this.addNewLocation} value={this.state.newLocation}/>
-              </div>
-              <div className="col-sm-2">
-                <button className="btn btn-primary" onClick={this.addLocation}>Add Location</button>
+          <div className="form-group">
+            <label className="col-sm-2 control-label">New Location</label>
+            <div className="col-sm-12">
+              <div className="row">
+                <div className="col-sm-4">
+                  <input type="text" className="form-control" onChange={this.addNewLocation} value={this.state.newLocation}/>
+                </div>
+                <div className="col-sm-2">
+                  <button className="btn btn-primary" onClick={this.addLocation}>Add Location</button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="form-group">
-          <label className="col-sm-2 control-label">Added Locations</label>
-          <div className="col-sm-4">
-            <ul>
-              {
-                this.state.locations.map((e, i) => <li key={i}>{e}</li>)
-              }
-            </ul>
+          <div className="form-group">
+            <label className="col-sm-2 control-label">Added Locations</label>
+            <div className="col-sm-4">
+              <ul>
+                {
+                  this.state.locations.map((e, i) => <li key={i}>{e}</li>)
+                }
+              </ul>
+            </div>
           </div>
-        </div>
-        <div className="form-group">
-          <div className="col-sm-8">
-            <input type="submit" value="Search Elevations" className="btn btn-primary" onClick={this.search}/>
+          <div className="form-group">
+            <div className="col-sm-8">
+              <input type="submit" value="Search Elevations" className="btn btn-primary" onClick={this.search}/>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+        <ElevationList elevations={this.state.elevations} />
+      </div>
     );
   }
 }
