c59d6187dc15f9ce99ad3e8e3d3219ab3722337d
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-11-04 16:56:00 -0600

    alternate async

--- server.js
+++ server.js
@@ -16,16 +16,37 @@ app.use(function(req, res, next) {
 
 app.use(bodyParser.json());
 
-app.post('/geoCode', (req, res) => {
-  const elevations = [];
+app.post('/geoCode', async (req, res, next) => {
+  // const elevations = [];
   const key = process.env.KEY;
   const locations = req.body.locations;
 
-  const search = async () => {
-    return locations.map(city => {
+  // const search = async () => {
+  // async function search() {
+  //   return locations.map(city => {
+  //     // build first url with key and location data
+  //     const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${key}`;
+  //
+  //     return fetch(url).then(res => res.json()).then(geoCode => {
+  //       // take values for lat and lng and build url with them
+  //       const lat = geoCode.results[0].geometry.location.lat;
+  //       const lng = geoCode.results[0].geometry.location.lng;
+  //       const url2 = `https://maps.googleapis.com/maps/api/elevation/json?locations=${lat},${lng}&key=${key}`;
+  //       return fetch(url2).then(response => response.json()).then(elev => {
+  //         const elevation = elev.results[0].elevation;
+  //         // elevations.push([city, elevation]);
+  //         return [city, elevation];
+  //       });
+  //     });
+  //   });
+  // }
+
+    const elevations = locations.map(city => {
+      // build first url with key and location data
       const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${key}`;
 
       return fetch(url).then(res => res.json()).then(geoCode => {
+        // take values for lat and lng and build url with them
         const lat = geoCode.results[0].geometry.location.lat;
         const lng = geoCode.results[0].geometry.location.lng;
         const url2 = `https://maps.googleapis.com/maps/api/elevation/json?locations=${lat},${lng}&key=${key}`;
@@ -36,15 +57,17 @@ app.post('/geoCode', (req, res) => {
         });
       });
     });
-  }
 
 
-  search().then(() => {
+
     console.log(elevations);
-    res.send(JSON.stringify(elevations));
-  });
+    // res.send(JSON.stringify(elevations));
+    next(elevations)
 
+}).use((elevations) => {
+  console.log(elevations);
 });
+
 app.use(express.static(__dirname + '/public'));
 
 
