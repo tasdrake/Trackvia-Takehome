2140f646773650f4550bbc20fbe3368a92791d8a
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-11-04 17:39:37 -0600

    change result from function

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.post('/geoCode', (req, res, next) => {
   const locations = req.body.locations;
 
   function search() {
-    return new Promise((resolve, reject) => {
+    new Promise((resolve, reject) => {
       const elevations = locations.map(city => {
         // build first url with key and location data
         const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${key}`;
