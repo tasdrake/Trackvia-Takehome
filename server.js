d187d46ad26be463baa480996ffefbeeab386b78
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-11-04 17:06:48 -0600

    added promise

--- server.js
+++ server.js
@@ -16,24 +16,6 @@ app.use(function(req, res, next) {
 
 app.use(bodyParser.json());
 
-async function search(locations, key) {
-  return locations.map(city => {
-    // build first url with key and location data
-    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${key}`;
-
-    return fetch(url).then(res => res.json()).then(geoCode => {
-      // take values for lat and lng and build url with them
-      const lat = geoCode.results[0].geometry.location.lat;
-      const lng = geoCode.results[0].geometry.location.lng;
-      const url2 = `https://maps.googleapis.com/maps/api/elevation/json?locations=${lat},${lng}&key=${key}`;
-      return fetch(url2).then(response => response.json()).then(elev => {
-        const elevation = elev.results[0].elevation;
-        // elevations.push([city, elevation]);
-        return [city, elevation];
-      });
-    });
-  });
-}
 
 app.post('/geoCode', async (req, res, next) => {
   // const elevations = [];
@@ -77,9 +59,33 @@ app.post('/geoCode', async (req, res, next) => {
     //   });
     // });
 
-    search(locations, key).then(e => {
-      console.log(e);
-    });
+     function search() {
+      return new Promise((resolce, reject) => {
+        locations.map(city => {
+          // build first url with key and location data
+          const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${key}`;
+
+          return fetch(url).then(res => res.json()).then(geoCode => {
+            // take values for lat and lng and build url with them
+            const lat = geoCode.results[0].geometry.location.lat;
+            const lng = geoCode.results[0].geometry.location.lng;
+            const url2 = `https://maps.googleapis.com/maps/api/elevation/json?locations=${lat},${lng}&key=${key}`;
+            return fetch(url2).then(response => response.json()).then(elev => {
+              const elevation = elev.results[0].elevation;
+              // elevations.push([city, elevation]);
+              return [city, elevation];
+            });
+          });
+        });
+      });
+    }
+
+    search().then(() => {
+      console.log('finally');
+    })
+    // search(locations, key).then(e => {
+    //   console.log(e);
+    // });
 
 
     console.log(elevations);
